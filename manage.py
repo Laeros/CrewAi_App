df9798e8142a51845a325ae30d25203aef20477b
Laeros <104449811+Laeros@users.noreply.github.com>
2025-06-24 14:21:21 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,12 +2,15 @@ from app import create_app, db
 from flask.cli import FlaskGroup
 
 app = create_app()
-cli = FlaskGroup(app)
+cli = FlaskGroup(create_app=create_app)
 
+# Comando personalizado para crear la base de datos
 @cli.command("create_db")
 def create_db():
-    db.create_all()
-    print("Base de datos creada.")
+    with app.app_context():
+        db.create_all()
+        print("✅ Base de datos creada correctamente.")
 
+# Ejecutar CLI si se llama directamente el script
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    cli()
